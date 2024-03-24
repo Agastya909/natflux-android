233696c82d43bab6a56e48be178835ec52349047
Agastya <agastya.r@pickmywork.com>
2024-03-24 17:23:58 +0530

    add icon to bottom tab

--- src/navigation/BottomTabStack.tsx
+++ src/navigation/BottomTabStack.tsx
@@ -1,16 +1,53 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen, ProfileScreen, SearchScreen } from "../screens/index";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StackNavigatorType } from "../types";
+import * as OutlineIcon from "react-native-heroicons/outline";
+import { useTheme } from "@react-navigation/native";
 
 const Tabs = createBottomTabNavigator();
 const Stack = createNativeStackNavigator<StackNavigatorType>();
 
 const TabsNavigator: React.FC = () => {
+  const { colors } = useTheme();
   return (
-    <Tabs.Navigator screenOptions={{ headerShown: false }}>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: { borderColor: colors.text },
+        tabBarShowLabel: false,
+        tabBarIcon: ({ focused }) => {
+          const routeName = route.name;
+          if (routeName === "Home") {
+            return (
+              <OutlineIcon.HomeIcon
+                color={focused ? colors.primary : colors.text}
+                strokeWidth={focused ? 2 : 1}
+                size={28}
+              />
+            );
+          }
+          if (routeName === "Search") {
+            return (
+              <OutlineIcon.MagnifyingGlassIcon
+                color={focused ? colors.primary : colors.text}
+                strokeWidth={focused ? 2 : 1}
+                size={28}
+              />
+            );
+          }
+          if (routeName === "Profile") {
+            return (
+              <OutlineIcon.UserCircleIcon
+                color={focused ? colors.primary : colors.text}
+                strokeWidth={focused ? 2 : 1}
+                size={28}
+              />
+            );
+          }
+        }
+      })}>
       <Tabs.Screen name="Home" component={HomeScreen} />
       <Tabs.Screen name="Search" component={SearchScreen} />
       <Tabs.Screen name="Profile" component={ProfileScreen} />
